4a28413f97177c540321e2ccb6910558e3f0d246
dozyio <github@dozy.io>
2023-05-28 11:32:30 +0100

    telescope

--- init.lua
+++ init.lua
@@ -41,6 +41,7 @@ require("lazy").setup({
   -- Navigation
   { "kyazdani42/nvim-tree.lua", event = "VeryLazy", },
   { "nvim-telescope/telescope.nvim", event = "VeryLazy", },
+  { "nvim-telescope/telescope-fzy-native.nvim", event = "VeryLazy", },
 
   -- Winbar & Buffers
   { "akinsho/bufferline.nvim", event = "VeryLazy", },
@@ -66,22 +67,22 @@ require("lazy").setup({
 })
 
 -- Mouse
-vim.opt.mouse = ""
+vim.o.mouse = ""
 
 -- Tabs
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 0
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smarttab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 0
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.smarttab = true
 
 -- Indenting
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+vim.o.autoindent = true
+vim.o.smartindent = true
 
 -- Line Numbering
-vim.opt.relativenumber = true
-vim.opt.number = true
+vim.o.relativenumber = true
+vim.o.number = true
 vim.keymap.set("n", "<C-n>", ":set invrelativenumber<CR>")
 
 -- Text
@@ -101,23 +102,23 @@ vim.keymap.set("n", "<leader>d", "V\"_d", { noremap = true, nowait = true, silen
 vim.keymap.set("v", "<leader>d", "\"_d", { noremap = true, nowait = true, silent = true })
 
 -- Spelling
-vim.opt.spell = false
+vim.o.spell = false
 
 -- Window
-vim.opt.title = true
+vim.o.title = true
 
 -- Files
-vim.opt.fileformats = "unix"
+vim.o.fileformats = "unix"
 
 -- Project specific config files
 -- execute .nvimrc files in current dir
-vim.opt.exrc = true
+vim.o.exrc = true
 
 -- Undo
-vim.opt.undofile = true
-vim.opt.undolevels = 1000
-vim.opt.undoreload = 10000
-vim.opt.undodir = os.getenv('HOME') .. "/.local/state/nvim/undo/"
+vim.o.undofile = true
+vim.o.undolevels = 1000
+vim.o.undoreload = 10000
+vim.o.undodir = os.getenv('HOME') .. "/.local/state/nvim/undo/"
 -- additional undo breakpoints
 vim.keymap.set("i", ",", ",<c-g>u", { noremap = true, nowait = true, silent = true })
 vim.keymap.set("i", ".", ".<c-g>u", { noremap = true, nowait = true, silent = true })
@@ -131,36 +132,37 @@ vim.keymap.set("i", "[", "[<c-g>u", { noremap = true, nowait = true, silent = tr
 vim.keymap.set("i", "]", "]<c-g>u", { noremap = true, nowait = true, silent = true })
 
 -- Search
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
-vim.opt.showmatch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+vim.o.incsearch = true
+vim.o.hlsearch = true
+vim.o.showmatch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 -- keep cursor centered when searching
 vim.keymap.set("n", "n", "nzzzv", { noremap = true, nowait = true, silent = true })
 vim.keymap.set("n", "N", "Nzzzv", { noremap = true, nowait = true, silent = true })
 -- search and replace current word
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/")
+vim.keymap.set("n", "<leader><space>", ":nohlsearch<CR>")
 
 -- Scrolling
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 5
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 5
 
 -- Visual move
 vim.keymap.set("v", "J", "nzzzv", { noremap = true, nowait = true, silent = true })
 vim.keymap.set("v", "K", "Nzzzv", { noremap = true, nowait = true, silent = true })
 
 -- Spacing
-vim.opt.listchars = ""
-vim.opt.list = false
+vim.o.listchars = ""
+vim.o.list = false
 -- vim.cmd("let c_space_errors 1")
 
 -- Cursor
 -- block cursor in insert mode
-vim.opt.guicursor = ""
+vim.o.guicursor = ""
 
 -- Columns
-vim.opt.colorcolumn = "80,120"
+vim.o.colorcolumn = "80,120"
 
 -- Navigation
 vim.cmd("set nostartofline")
@@ -173,11 +175,11 @@ vim.keymap.set("n", "<c-x>", ":bd<CR>", { noremap = true, silent = true })
 
 -- Misc
 -- disable inline vim commands in files
-vim.opt.modelines = 0
+vim.o.modelines = 0
 -- for status bars
-vim.opt.winminheight = 0
+vim.o.winminheight = 0
 -- save previous commands, history etc
-vim.opt.shada = "!,'100,<100,:100,s10,%,h" -- see :h 'shada'
+vim.o.shada = "!,'100,<100,:100,s10,%,h" -- see :h 'shada'
 
 require("lua/git/init")
 require("lua/navigation/init")
--- lua/navigation/init.lua
+++ lua/navigation/init.lua
@@ -1,4 +1,4 @@
--- nvim-tree
+-- Nvim-Tree
 require("nvim-tree").setup({
   filters = { custom = { "^.git$" } }
 })
@@ -19,6 +19,7 @@ wk.register({
   },
 })
 
+-- Close if nvim-tree is last buffer
 vim.api.nvim_create_autocmd("QuitPre", {
   callback = function()
     local invalid_win = {}
@@ -35,3 +36,46 @@ vim.api.nvim_create_autocmd("QuitPre", {
     end
   end
 })
+
+-- Telescope
+local sorters = require("telescope.sorters")
+local previewers = require("telescope.previewers")
+local actions = require("telescope.actions")
+local builtin = require('telescope.builtin')
+
+require("telescope").setup({
+    defaults = {
+        file_sorter = sorters.get_fzy_sorter,
+        prompt_prefix = " >",
+        color_devicons = true,
+
+        file_previewer = previewers.vim_buffer_cat.new,
+        grep_previewer = previewers.vim_buffer_vimgrep.new,
+        qflist_previewer = previewers.vim_buffer_qflist.new,
+
+        mappings = {
+            i = {
+                ["<C-x>"] = false,
+                ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+            },
+        },
+        file_ignore_patterns = { ".git/.*", },
+    },
+    extensions = {
+        fzy_native = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+        },
+    },
+    pickers = {
+      find_files = {
+        hidden = true,
+      },
+    },
+})
+
+require("telescope").load_extension("fzy_native")
+
+-- Actions
+vim.keymap.set('n', '<c-p>', builtin.find_files, { noremap = true, nowait = true, silent = true })
+vim.keymap.set('n', '<c-g>', builtin.live_grep, { noremap = true, nowait = true, silent = true })
