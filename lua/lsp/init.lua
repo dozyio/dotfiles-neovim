2d46f1e2170573ee3a9488e99137249ed8d712e6
dozyio <github@dozy.io>
2024-01-31 00:03:26 +0000

    chore: fix dir golangci

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -10,7 +10,7 @@ require'lspconfig'.golangci_lint_ls.setup{
 	filetypes = {'go', 'gomod'},
   root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
   init_options = {
-      command = { "golangci-lint", "run", "-c", home .. ".golangci.yml", "--out-format", "json", "--issues-exit-code=1" };
+      command = { "golangci-lint", "run", "-c", home .. "/.golangci.yml", "--out-format", "json", "--issues-exit-code=1" };
   }
 }
 
