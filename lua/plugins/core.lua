fc34c447a9d1e8cfdc1b492351f66c330e005bf5
dozyio <github@dozy.io>
2025-02-23 01:18:37 +0000

    simple autocomplete keymaps, smaller func sigs

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -27,8 +27,30 @@ return {
     "saghen/blink.cmp",
     opts = {
       keymap = {
-        preset = "enter",
-        ["<C-c>"] = { "cancel", "fallback" },
+        preset = "none",
+        ["<up>"] = { "select_prev", "fallback" },
+        ["<down>"] = { "select_next", "fallback" },
+        ["<cr>"] = { "accept", "fallback" },
+        ["<tab>"] = { "select_and_accept", "fallback" },
+      },
+      completion = {
+        menu = {
+          auto_show = false,
+        },
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = false,
+          },
+        },
+        ghost_text = {
+          enabled = true,
+          show_with_selection = true,
+          show_without_selection = true,
+        },
+      },
+      signature = {
+        enabled = true,
       },
     },
   },
@@ -62,5 +84,13 @@ return {
         show_buffer_close_icons = false,
       },
     },
+  },
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      opts.lsp.signature = {
+        auto_open = { enabled = false },
+      }
+    end,
   },
 }
